d96e9676e73b393698a798f7ec8d9bc2e2a56133
Andrew <andrewy3295@gmail.com>
2022-12-11 16:53:22 -0600

    Finishing up the badges and github user

---
+++ Develop/README.md
@@ -0,0 +1,18 @@
+# r                         ##Apache License 2.0
+
+## Description
+    r
+## Table of Contents
+    r
+## Installation
+    r
+## Usage
+    r
+## Liscense
+    The current licence is use is Apache License 2.0.
+## Contributing
+    r
+## Tests
+    r
+## Questions
+    r
--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,32 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = require("fs")
+const fs = require("fs");
+const { create } = require("domain");
+const {writeFile} = require('fs').promises;
 
 // TODO: Create an array of questions for user input
 // const questions = [];
 
-function generateReadme(title, description, table_of_contents, installation, usage, license, contributing, tests, questions){
-    `# <${title}>
-    ## Description
-        ${description}
-    ## Table of Contents
-        ${table_of_contents}
-    ## Installation
-        ${installation}
-    ## Usage
-        ${usage}
-    ## Liscense
-        ${license}
-    ## Contributing
-        ${contributing}
-    ## Tests
-        ${tests}
-    ## Questions
-        ${questions}`
-}
+const generateReadme = ({title, description, table_of_contents, installation, usage, license, contributing, tests, questions, badge}) =>
+`# ${title}
+
+## Description
+    ${description}
+## Table of Contents
+    ${table_of_contents}
+## Installation
+    ${installation}
+## Usage
+    ${usage}
+## Liscense
+    The current licence in use is ${license}.
+## Contributing
+    ${contributing}
+## Tests
+    ${tests}
+## Questions
+    ${questions}`
+;
 
 // function Question(title, description, table_of_contents, installation, usage, license, contributing, tests, questions) {
 //     this.title = title;
@@ -66,9 +69,10 @@ inquirer
             name: 'usage',
         },
         {
-            type: 'input',
+            type: 'list',
             message: 'What type of license will you be using for this application?',
             name: 'license',
+            choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", `BSD 2-Clause "Simplified" License`, `BSD 3-Clause "New" or "Revised" License`, "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"],
         },
         {
             type: 'input',
@@ -87,13 +91,56 @@ inquirer
         },
     ])
     .then((info) => {
-        const readmeContent = generateReadme(info)
+        createBadge(info.license)
+        const readmeContent = generateReadme(info, badge)
         fs.writeFile("README.md", readmeContent, (err) =>
             err ? console.log(err): console.log("Successfully created your README.md file."))
     })
+;
 
-
-
+// On the to do list to finish up. Most of below is not correctly formatted
+function createBadge() {
+    let badge = ""
+    if (license === `Apache License 2.0`){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "GNU General Public License v3.0"){
+        badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+        }
+    else if (license === "MIT License"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === `BSD 2-Clause "Simplified" License`){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === `BSD 3-Clause "New" or "Revised" License`){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "Boost Software License 1.0"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "Creative Commons Zero v1.0 Universal"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "Eclipse Public License 2.0"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "GNU Affero General Public License v3.0"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "GNU General Public License v2.0"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "GNU Lesser General Public License v2.1"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "Mozilla Public License 2.0"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }
+    else if (license === "The Unlicense"){
+        badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        }                
+}
 
 
 
